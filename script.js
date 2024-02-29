056c759d6e609fb957425327e6ab50c0953ed8e2
Draunt <100187574+JackOdwyer@users.noreply.github.com>
2024-03-01 00:14:45 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+function convert() {
+    const inputText = document.getElementById('inputText').value;
+    const outputText = stringToUnicode(inputText);
+    document.getElementById('outputText').value = outputText;
+}
+
+function stringToUnicode(str) {
+    const unicodeArray = [];
+
+    for (let i = 0; i < str.length; i++) {
+        const charCode = str.charCodeAt(i);
+        unicodeArray.push('U+' + charCode.toString(16).toUpperCase());
+    }
+
+    return unicodeArray.join(' ');
+}
 
